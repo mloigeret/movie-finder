a27cb9f9c6861709f5ee7f17bdcf2c7f7a908f13
Manuel Loigeret <Manuel.loigeret@gmail.com>
2021-07-28 18:26:49 -0400

    Refactor UIDownlaodableImageView to use Rx. Also prevents image flickering

--- MovieFinder/Views/UIDownloadableImageView.swift
+++ MovieFinder/Views/UIDownloadableImageView.swift
@@ -6,11 +6,52 @@
 //
 
 import UIKit
+import RxSwift
 
-class UIDownloadableImageView: UIImageView {
-    let imageCache = NSCache<NSString, AnyObject>()
-    var imageURLString: String?
+class UIDownloadableImageView: UIView {
+    private let _imv = UIImageView()
+    private let _imageCache = NSCache<NSString, AnyObject>()
+    private let _imageURLObservable = BehaviorSubject<URL?>(value: nil)
+    
+    private let _disposeBag = DisposeBag()
+    
+    init() {
+        super.init(frame: .zero)
+        configureLayout()
+        configureRx()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureLayout() {
+        _imv.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(_imv)
 
+        NSLayoutConstraint.activate([
+            _imv.leftAnchor.constraint(equalTo: self.leftAnchor),
+            _imv.rightAnchor.constraint(equalTo: self.rightAnchor),
+            _imv.topAnchor.constraint(equalTo: self.topAnchor),
+            _imv.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func configureRx() {
+        _imageURLObservable
+            .flatMapLatest { url -> Observable<Data>  in
+                guard let url = url else { return Observable.of(Data.init()) }
+                let request = URLRequest.init(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] data in
+                guard let img = UIImage(data: data) else { return }
+                _imv.image = img
+            })
+            .disposed(by: _disposeBag)
+    }
+    
     func downloadImageFrom(urlString: String?,
                            placeholder: UIImage?,
                            imageMode: UIView.ContentMode) {
@@ -24,19 +65,13 @@ class UIDownloadableImageView: UIImageView {
     func downloadImageFrom(url: URL,
                            placeholder: UIImage?,
                            imageMode: UIView.ContentMode) {
-        image = placeholder
+        _imv.image = placeholder
         contentMode = imageMode
-        if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) as? UIImage {
-            self.image = cachedImage
-        } else {
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                guard let data = data, error == nil else { return }
-                DispatchQueue.main.async {
-                    let imageToCache = UIImage(data: data)
-                    self.imageCache.setObject(imageToCache!, forKey: url.absoluteString as NSString)
-                    self.image = imageToCache
-                }
-            }.resume()
+        if let cachedImage = _imageCache.object(forKey: url.absoluteString as NSString) as? UIImage {
+            _imv.image = cachedImage
+        }
+        else {
+            _imageURLObservable.onNext(url)
         }
     }
 }
